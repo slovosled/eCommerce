b06cb88e41817119e540b2bbe8ca9cb7180c0cb8
Pavol Podstreleny <palo.podstreleny@gmail.com>
2020-08-21 11:08:55 +0200

    Updated security constants, Added implementation of UserDetailService and added WebSecurityConfiguration class

---
+++ src/main/java/com/example/demo/security/UserDetailServiceImpl.java
@@ -0,0 +1,25 @@
+package com.example.demo.security;
+
+import com.example.demo.model.persistence.User;
+import com.example.demo.model.persistence.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(s);
+        if (user == null) {
+            throw new UsernameNotFoundException(s);
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), Collections.emptyList());
+    }
+}
